4450e768de62f69fff77d6dad38460677f3cea7e
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-04-04 10:47:34 -0300

    Criando menu fundamentos

--- src/app/menu/menuPrincipal.ts
+++ src/app/menu/menuPrincipal.ts
@@ -1,5 +1,6 @@
 import TerminalUtil from "@/util/TerminalUtil";
 import { terminal } from "terminal-kit";
+import menuFundamentos from "./menuFundamentos";
 
 export default async function menuPrincipal() {
     TerminalUtil.titulo('Menu Principal');
@@ -11,6 +12,7 @@ export default async function menuPrincipal() {
 
     switch(resposta.selectedIndex) {
         case 0:
+            await menuFundamentos();
             break;
         default:
             process.exit(0);
