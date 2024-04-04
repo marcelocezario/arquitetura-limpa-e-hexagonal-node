abbd6c56d56cb81e82298c9c2526a41a0ad82d88
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-04-04 17:20:06 -0300

    Iniciando express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
+
+import express from 'express';
+
+const app = express();
+const porta = process.env.API_PORT ?? 4000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(porta, () => {
+    console.log(`🔥 Servidor executando na porta ${porta}`);
+})
