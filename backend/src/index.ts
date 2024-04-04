024182d672fd51d9eb200aeddc37110d46de40e7
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-04-04 20:44:55 -0300

    Criado rota registro de usuário

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import RegistrarUsuario from './core/usuario/service/RegistrarUsuario';
+import RepositorioUsuarioPg from './external/db/RepositorioUsuarioPg';
+import SenhaCripto from './external/auth/SenhaCripto';
+import RegistrarUsuarioController from './external/api/RegistrarUsuarioController';
 
 const app = express();
 const porta = process.env.API_PORT ?? 4000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.listen(porta, () => {
     console.log(`🔥 Servidor executando na porta ${porta}`);
 })
+
+// ----------------------------------------- Rotas Abertas
+
+const repositorioUsuario = new RepositorioUsuarioPg();
+const provedorCripto = new SenhaCripto();
+const registrarUsuario = new RegistrarUsuario(repositorioUsuario, provedorCripto);
+
+new RegistrarUsuarioController(app, registrarUsuario);
