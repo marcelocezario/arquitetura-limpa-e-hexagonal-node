cd915fe89849e2d52015aacb508560b33fc8cd1c
Marcelo Henrique Cezario <marcelocezario@gmail.com>
2024-04-04 14:06:39 -0300

    Caso de uso: Registrar Usuário - parte 1

---
+++ src/core/usuario/service/RegistrarUsuario.ts
@@ -0,0 +1,10 @@
+import CasoDeUso from "@/core/shared/CasoDeUso";
+import Usuario from "../model/Usuario";
+
+export default class RegistrarUsuario implements CasoDeUso<Usuario, void> {
+
+    async executar(usuario: Usuario): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+
+}
